16208eacc85f773447867a38e7a1702c2ab0fd59
Luis Guilherme Passos Ramos <129339010+LuisPassosRamos@users.noreply.github.com>
2025-07-23 08:10:25 -0300

    Add files via upload

---
+++ api.blockchain.contracts/src/main/java/edu/ifba/saj/ads/api/blockchain/contracts/Application.java
@@ -0,0 +1,13 @@
+package edu.ifba.saj.ads.api.blockchain.contracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
